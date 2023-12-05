949217dc4a328ee9c644bc6a1c37f07ac8ef1d36
Mateusz Mulka <96475096+kottoization@users.noreply.github.com>
2023-12-05 17:47:33 +0100

    Add files via upload

---
+++ Description.py
@@ -0,0 +1,2 @@
+# Hello there 😁 welcome to my ML repo where you can find many ML techniques hosted in different branches.
+# After finishing my bigger academic ML project I will upload it to this main branch ;) 
